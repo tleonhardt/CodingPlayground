86a43580339fea09a1ce63aa22012ef5e983b2c4
Todd Leonhardt <todd.leonhardt@gmail.com>
2016-09-08 19:42:40 -0400

    Added forest fire example
    Added forest fire example for basic plotting

---
+++ dataquest/DataVisualiation/BasicPlotting/basic_plotitng.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""
+This analyze data on forest firest from a National Park in Portugal.  The park is divided up into a
+9 by 9 grid. Each fire has corresponding X position on the grid and Y position on the grid
+
+Each row describes a fire that happened in Montesinho National Park. Here's a listing of the columns:
+    X -- The X position on the grid where the fire occurred.
+    Y -- The Y position on the grid where the fire occured.
+    month -- the month the fire occcurred.
+    day -- the day of the week the fire occurred.
+    temp -- the temperature in Celsius when the fire occurred.
+    wind -- the wind speed when the fire occurred in units of km/h
+    rain -- the rainfall when the fire occurred.
+    area -- the area the fire consumed in ha
+
+The intent is to gain some familiarity with Matplotlib
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Use Pandas to read the CSV file into a DataFrame
+forest_fires = pd.read_csv('forest_fires.csv')
+
+# Enable interactive mode so plt.show() won't block
+plt.ion()
+
+
+## Scatter Plots
+# Make a scatter plot with the wind column on the x-axis and the area column on the y-axis
+plt.figure()
+plt.scatter(forest_fires['wind'], forest_fires['area'])
+plt.xlabel('wind speed (km/h)')
+plt.ylabel('burned area (ha)')
+plt.show()
+
+# Make a scatter plot with the temp column on the x-axis and the area column on the y-axis
+plt.figure()
+plt.scatter(forest_fires['temp'], forest_fires['area'])
+plt.xlabel('temperature (C)')
+plt.ylabel('burned area (ha)')
+plt.show()
+
+
+## Line Charts
+age = [5, 10, 15, 20, 25, 30]
+height = [25, 45, 65, 75, 75, 75]
+# Use the plot() method to plot age on the x-axis and height on the y-axis
+plt.figure()
+plt.plot(age, height)
+plt.xlabel('age (years)')
+plt.xlabel('height (inches)')
+plt.show()
+
+
+## Bar Graphs
+# Use pivot_table() method to calculate the average area of the fires started at each X or Y position
+area_by_y = forest_fires.pivot_table(index="Y", values="area", aggfunc=np.mean)
+area_by_x = forest_fires.pivot_table(index="X", values="area", aggfunc=np.mean)
+# Use the bar() method to plot area_by_y.index on the x-axis and area_by_y on the y-axis
+plt.figure()
+plt.bar(area_by_y.index, area_by_y)
+plt.xlabel('Y grid location')
+plt.ylabel('Average area of fires started at location Y')
+plt.show()
+# Use the bar() method to plot area_by_y.index on the x-axis and area_by_y on the y-axis
+plt.figure()
+plt.bar(area_by_x.index, area_by_x)
+plt.xlabel('X grid location')
+plt.ylabel('Average area of fires started at location X')
+plt.show()
+
+
+## Horizontal Bar Graphs
