9c8d0f55bf1af783ce7af64dd9f0552271373f59
Todd Leonhardt <todd.leonhardt@gmail.com>
2016-09-22 23:03:28 -0400

    Added a few more optional arguments to the plt.axvline() example

--- python/matplotlib/vertical_line.py
+++ python/matplotlib/vertical_line.py
@@ -7,6 +7,6 @@ import matplotlib.pyplot as plt
 
 values = np.random.randn(100)
 plt.hist(values, label='values')
-plt.axvline(values.mean(), c='red', label='mean')
+plt.axvline(values.mean(), color='red', linestyle='--', linewidth=4, label='mean')
 plt.legend()
 plt.show()
