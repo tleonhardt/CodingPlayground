6270a6d1b9a2497bc471c76e9aea426733ef4935
Todd Leonhardt <todd.leonhardt@gmail.com>
2016-12-14 16:28:54 -0500

    Added example of using CMake with SWIG and Python

---
+++ python/swig/cmake/example.h
@@ -0,0 +1,10 @@
+/* File : example.h
+ *
+ * Header declaring the public interface.
+ */
+
+#pragma once
+extern double My_variable;
+extern int fact(int n);
+extern int my_mod(int x, int y);
+extern char *get_time();
