497ccea77edf931654deb8631d7f92916ad99887
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-01-19 20:48:36 -0500

    Added trivial python script to purposely throw an exception

---
+++ python/exception/should_throw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+This file should throw a TypeError exception and generate a Traceback.
+"""
+an_int = 5
+a_string = 'hello'
+
+# This line should throw a TypeError exception because you can't add an int and a str together
+new_var = an_int + a_string
