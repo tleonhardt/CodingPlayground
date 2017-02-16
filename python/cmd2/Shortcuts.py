5469ac7798130da01e4791bdc62a6e3870481b84
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-02-15 23:31:37 -0500

    Made a couple small updates

---
+++ python/cmd2/Shortcuts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""A sample application for cmd2 as it is completely out of the box.
+"""
+
+from cmd2 import Cmd, make_option, options
+
+
+class CmdLineApp(Cmd):
+    """ Example cmd2 application. """
+    Cmd.shortcuts.update({'p': 'help', 'pcon': 'help history'})
+
+if __name__ == '__main__':
+    c = CmdLineApp()
+    c.cmdloop()
