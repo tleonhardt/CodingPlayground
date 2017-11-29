a9877a6dd429c226ce2c9a84f1f2994b74d62ad2
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-11-28 20:17:05 -0500

    Force zstandard example to use Python 3 by default

--- python/compression/hello_zstandard.py
+++ python/compression/hello_zstandard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding=utf-8
 """
 Uses python-zstandard wrapper around zstandard C library (simplest usage of API for round-trip compress/decompress)
