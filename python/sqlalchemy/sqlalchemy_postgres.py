73d8bc11360b5b0b58e81016cf9c30ccccbb6899
Todd Leonhardt <todd.leonhardt@gmail.com>
2018-02-04 13:48:54 -0500

    Added excpetion handling when connecting to PostgreSQL server fails

--- python/sqlalchemy/sqlalchemy_postgres.py
+++ python/sqlalchemy/sqlalchemy_postgres.py
@@ -3,14 +3,19 @@
 """This is a simple example of using SQLAlchemy in ORM mode to work with a PostgreSQL database.
 """
 import getpass
+import logging
 import sys
 
 import colorama
 from colorama import Fore
-import sqlalchemy as sa
-import sqlalchemy.ext.declarative as sa_decl
-import sqlalchemy.orm as sa_orm
+import sqlalchemy as sa                         # SQLAlchemy
+import sqlalchemy.exc as sa_exc                 # SQLAlchemy Exceptions
+import sqlalchemy.ext.declarative as sa_decl    # SQLAlchemy Declarative
+import sqlalchemy.orm as sa_orm                 # SQLAlchemy ORM
 import sqlalchemy_utils as sa_utils
+
+logger = logging.getLogger(__name__)
+
 
 # Classes mapped using the Declarative system are defined in terms of a base class which maintains a catalog of
 # classes and tables relative to that base - this is known as the declarative base class
@@ -59,6 +64,58 @@ class Address(Base):
 User.addresses = sa_orm.relationship("Address", order_by=Address.id, back_populates="user")
 
 
+def check_db_schema(declarative_base, sa_session):
+    """Check whether the current database matches the models declared in model base.
+
+    Checks to make sure all tables exist and that each table contains columns with the expected name.
+
+    What is not checked:
+    * Column types are not verified
+    * Relationships are not verified at all (TODO)
+
+    :param declarative_base: Declarative Base for SQLAlchemy models to check
+    :param sa_session: SQLAlchemy session bound to an engine
+    :return: True if all declared models have corresponding tables and columns
+    """
+    sa_engine = sa_session.get_bind()
+    iengine = sa.inspect(sa_engine)
+
+    errors = False
+
+    tables = iengine.get_table_names()
+
+    # Go through all SQLAlchemy models
+    for name, klass in declarative_base._decl_class_registry.items():
+
+        if isinstance(klass, sa_decl._ModuleMarker):
+            # Not a model
+            continue
+
+        table = klass.__tablename__
+        if table in tables:
+            # Check all columns are found
+            # Looks like [{'default': "nextval('sanity_check_test_id_seq'::regclass)", 'autoincrement': True, 'nullable': False, 'type': INTEGER(), 'name': 'id'}]
+
+            columns = [c["name"] for c in iengine.get_columns(table)]
+            mapper = sa.inspect(klass)
+
+            for column_prop in mapper.attrs:
+                if isinstance(column_prop, sa_orm.RelationshipProperty):
+                    # TODO: Add sanity checks for relations
+                    pass
+                else:
+                    for column in column_prop.columns:
+                        # Assume normal flat column
+                        if not column.key in columns:
+                            logger.error("Model %s declares column %s which does not exist in database %s", klass, column.key, sa_engine)
+                            errors = True
+        else:
+            logger.error("Model %s declares table %s which does not exist in database %s", klass, table, sa_engine)
+            errors = True
+
+    return not errors
+
+
 if __name__ == '__main__':
     colorama.init(autoreset=True)
 
@@ -67,14 +124,14 @@ if __name__ == '__main__':
     assert sa_ver >= 1.015
     print(Fore.LIGHTBLUE_EX + 'Using SQLAlchemy version {}'.format(sa.__version__))
 
-    # User name and password for the PostgreSQL server
-    db_user = getpass.getuser()
-    db_pass = getpass.getpass(prompt="{}'s PostgreSQL password: ".format(db_user))
-
     # PostgreSQL server host:port - if the port is the default of 5432, then it can be left off
     host = 'localhost'
 
-    # # WARNING: This database must already exist in the PostgreSQL server.  It may or may not contain any tables.
+    # User name and password for the PostgreSQL server
+    db_user = getpass.getuser()
+    db_pass = getpass.getpass(prompt="{}'s password for PostgreSQL server {!r}: ".format(db_user, host))
+
+    # Database to connect to within the PostgreSQL server.  It may or may not contain any tables.
     database = 'sample_db'
     if len(sys.argv) > 1:
         database = sys.argv[1]
@@ -82,9 +139,16 @@ if __name__ == '__main__':
     #  The string form of the URL is ``dialect[+driver]://user:password@host:port/dbname[?key=value..]``
     db_url = 'postgresql://{}:{}@{}/{}'.format(db_user, db_pass, host, database)
 
+    # Try connecting to the server to check to see if the DB exists and our the URL and credentials are good
+    try:
+        db_exists = sa_utils.database_exists(db_url)
+    except sa_exc.OperationalError as err:
+        print(Fore.RED + 'Failed to connect to PostgreSQL server {!r} as user {!r}: {}'.format(host, db_user, err))
+        sys.exit(1)
+
     # Check to see if the database exists
     validate_shema = False
-    if sa_utils.database_exists(db_url):
+    if db_exists:
         # Warn the user that the DB exists and ask if they would like to delete it and recreate it or move on?
         print(Fore.YELLOW + 'Database {!r} already exists'.format(database))
         recreate = input('Would you like to delete database {!r} and recreate it from scratch (y/n)? ')
@@ -116,7 +180,12 @@ if __name__ == '__main__':
 
     # Validate the schema if the database already existed and the user opted not to recreate it from scratch
     if validate_shema:
-        # TODO: Validate the schema
+        pass
+        # if check_db_schema(Base, session):
+        #     print(Fore.LIGHTGREEN_EX + 'Existing DB schema looks OK')
+        # else:
+        #     print(Fore.RED + 'Existing DB schema conflicts with expected schema, database needs to be recreated')
+        #     sys.exit(2)
 
     # Configure details for a user
     user_name = 'todd'
